55b1e377753d873c1e5de17e6bb5750a3bee11e3
jessica <jvictoriaw@hotmail.co.uk>
2023-05-05 23:30:51 +0100

    added 'loading' when searching for the data

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ let weather = {
         const { icon, description } = data.weather[0];
         const { temp, temp_min, temp_max, humidity } = data.main;
         const { speed } = data.wind;
-        console.log(name,icon,description,temp,temp_min,temp_max,humidity,speed)
+
         document.querySelector(".city").innerText = "Weather in " + name;
         document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
         document.querySelector(".description").innerText = description;
@@ -23,18 +23,21 @@ let weather = {
         document.querySelector(".max-temp").innerText = "H: " + temp_max;
         document.querySelector(".humidity").innerText = "Humidity: " + humidity +"%";
         document.querySelector(".wind").innerText = "Wind: " + speed + "mph";
+        document.querySelector(".weather").classList.remove("loading");
     },
     search: function() {
         this.fetchWeather(document.querySelector('.searchbar').value);
     }
 };
 
-document.querySelector(".search button").addEventListener('click', function() {
+document.querySelector(".search button").addEventListener("click", function() {
     weather.search();
 });
 
-document.querySelector('searchbar').addEventListener('keyup', function(event) {
-    if (event.key == 'enter') {
+document.querySelector('.searchbar').addEventListener("keyup", function (event) {
+    if (event.key == 'Enter') {
         weather.search();
     }
-})
+});
+
+weather.fetchWeather("London");
